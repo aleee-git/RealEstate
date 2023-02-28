1bd38914d288b43081083f123b901d1cd71ff087
aleee-git <alejandraramirez.niu@nehemiahiu.com>
2023-02-28 14:52:46 -0600

    Navegacion Responsive

---
+++ src/js/app.js
@@ -0,0 +1,22 @@
+// Funcion para el menu de hamburguesa
+document.addEventListener('DOMContentLoaded', function () {
+    eventListener();
+});
+
+function eventListener () {
+    const mobile = document.querySelector('.mobile-menu');
+
+    mobile.addEventListener('click', responsive);
+}
+
+function responsive () {
+    const nav = document.querySelector('.navegacion');
+
+    // if (nav.classList.contains('mostrar')) {
+    //     nav.classList.remove('mostrar');
+    // } else {
+    //     nav.classList.add('mostrar');
+    // }
+    
+    nav.classList.toggle('mostrar');
+}
