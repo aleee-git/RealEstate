aa0c80c1ccf69fd1ff645e80bd4ba86bbd4c469d
aleee-git <alejandraramirez.niu@nehemiahiu.com>
2023-02-28 17:04:25 -0600

    Dark Mode

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,19 @@
 // Funcion para el menu de hamburguesa
 document.addEventListener('DOMContentLoaded', function () {
+
     eventListener();
+
+    darkMode();
 });
+
+function darkMode () {
+    const botonDark = document.querySelector('.dark-mode');
+
+    botonDark.addEventListener('click', function(){
+        document.body.classList.toggle('dark-mood');
+    });
+}
+
 
 function eventListener () {
     const mobile = document.querySelector('.mobile-menu');
