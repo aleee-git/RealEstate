ed2917ff58e662ab817589ea1e8dfd529e915fbd
aleee-git <alejandraramirez.niu@nehemiahiu.com>
2023-03-21 11:43:04 -0600

    Delete

--- crud/index.php
+++ crud/index.php
@@ -13,6 +13,30 @@ $result = mysqli_query($db, $query);
 // Mensaje de Alerta
 $mensaje = $_GET['mensaje'] ?? null;
 
+// Envia el id a eliminar
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $idPropiedad = $_POST['idPropiedad'];
+    $idPropiedad = filter_var($idPropiedad, FILTER_VALIDATE_INT);
+
+    if ($idPropiedad) {
+
+        // Elimina archivo
+        $query = "SELECT imagen FROM propiedades WHERE idPropiedad = $idPropiedad";
+        $resultado = mysqli_query($db, $query);
+        $propiedad = mysqli_fetch_assoc($resultado);
+
+        unlink("../imagenes/".$propiedad['imagen']);
+
+        // Elimina propiedad
+        $query = "DELETE FROM propiedades WHERE idPropiedad = $idPropiedad";
+        $resultado = mysqli_query($db, $query);
+        
+        if($resultado) {
+        header('Location: /realestate/crud/index.php?mensaje=3');
+        }
+    }
+}
+
 include '../includes/templates/header.php';
 ?>
 
@@ -22,6 +46,8 @@ include '../includes/templates/header.php';
             <p class="alerta exito">Property Registered</p>
         <?php elseif (intval($mensaje) === 2): ?>
             <p class="alerta exito">Property Updated</p>
+        <?php elseif (intval($mensaje) === 3): ?>
+            <p class="alerta exito">Property Deleted</p>
         <?php endif; ?>
         <a href="create.php" class="boton boton-verde" style="display: inline-block;">New Property</a>
 
@@ -45,7 +71,11 @@ include '../includes/templates/header.php';
                     <td> <img src="/realestate/imagenes/<?php echo $propiedad['imagen']; ?>" class="imagen-small"> </td>
                     <td> $ <?php echo $propiedad['precio'] ?> </td>
                     <td>
-                        <a href="#" class="boton-rojo-block">Delete</a>
+                        <form  method="POST" class="wp-100">
+                            <!-- Se pasa el id de la propiedad a eliminar -->
+                            <input type="hidden" name="idPropiedad" value="<?php echo $propiedad['idPropiedad']; ?>">
+                            <input class="boton-rojo-block" type="submit" value="Delete">
+                        </form>
                         <!-- Se lleva el id seleccionado para no perder la info -->
                         <a href="update.php?idPropiedad=<?php echo $propiedad['idPropiedad']  ?>" class="boton-amarillo-block">Update</a>
                     </td>
