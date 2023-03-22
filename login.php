3ea4880ae07274db7a4127de8da64e4fd0e3bf67
aleee-git <alejandraramirez.niu@nehemiahiu.com>
2023-03-22 10:16:01 -0600

    Vista Login

---
+++ login.php
@@ -0,0 +1,32 @@
+<?php 
+include './includes/templates/header.php'
+?>
+
+    <main class="contenedor seccion contenido-centrado">
+
+        <h2>Log in to Real Estate App</h2>
+
+        <form action="" class="formulario">
+
+            <fieldset>
+                <legend>🔒 Enter Your Email and Password 🔒</legend>
+
+                <label for="email">E-Mail</label>
+                <input type="email" id="email">
+
+                <label for="password">Password</label>
+                <input type="password" id="password">
+
+            </fieldset>
+
+            <input type="submit" value="Log in" class="boton boton-verde">
+
+        </form>
+
+
+    </main>
+
+    <?php 
+    include 'includes/templates/footer.php'
+    ?>
+    
