12b835c4278f5eced83b807dd1e201a4f6f4653c
aleee-git <alejandraramirez.niu@nehemiahiu.com>
2023-03-22 10:00:16 -0600

    Hashear Password

--- usuario.php
+++ usuario.php
@@ -4,12 +4,17 @@ include 'includes/templates/config/database.php';
 $db = conectarDB();
 
 // Crear User
-$email = "correo@correo.com";
-$password = "123456";
+$email = "Ale@correo.com";
+$password = "123456"; // en DB debe ser de 60 caracteres fijos para usar Hash
 
-$query = "INSERT INTO usuarios (email, password) VALUES ('$email', '$password');";
+// Hashear password - puede ser con PASSWORD_DEFAULT o PASSWORD_BCRYPT
+$passwordHash = password_hash($password, PASSWORD_BCRYPT);
+
+$query = "INSERT INTO usuarios (email, password) VALUES ('$email', '$passwordHash');";
 
 // Guardar en DB
 mysqli_query($db, $query);
+
+
 
 ?>
